8fc2a225810665aaa6ae8a2e89622ae42c1073fc
unknown <whmcconnell@protonmail.com>
2023-10-09 10:55:30 -0400

    Added my code, output file

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,5 @@
--- Add your SQL here
+select id as 'Movie_ID', name as 'Movie_Title', imdb_rating as 'Rating' 
+from movies
+where year <= 1985 and genre in('horror')
+order by Rating desc
+limit 3;
